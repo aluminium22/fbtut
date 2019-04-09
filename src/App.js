1cac944d5dda47994f5f061dbbfe2910f6bdff69
Lorin Lochridge <lorin.lochridge@matcotools.com>
2019-04-09 13:11:52 -0400

    Added layout and Dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Test</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path='/' component={Dashboard}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
