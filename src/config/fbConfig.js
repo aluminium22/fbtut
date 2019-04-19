29ec6537b5243e7f075804770b814b2b5c773676
aluminium22 <aluminiumloch@gmail.com>
2019-04-19 07:54:05 -0400

    Update fbConfig.js
    remove key

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,7 @@ import 'firebase/auth';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDikicMV30ss-91l8nUNi3bnHXaRv-njVA",
-  authDomain: "fbtut-e40a8.firebaseapp.com",
-  databaseURL: "https://fbtut-e40a8.firebaseio.com",
-  projectId: "fbtut-e40a8",
-  storageBucket: "fbtut-e40a8.appspot.com",
-  messagingSenderId: "630336635703"
+xxxxxxxxx
 };
 firebase.initializeApp(config);
 firebase.firestore();
